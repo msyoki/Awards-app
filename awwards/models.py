dfc3bc3674e3b6762129e49872e29fda2edd51eb
msyoki <msyokimutua@gmail.com>
2020-10-25 20:15:05 +0300

    add profile and project models

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_pic= models.ImageField(upload_to='profilepic/',null=True)
+    name= models.CharField(max_length=70)
+    bio= models.TextField()
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    webimage= models.ImageField(upload_to='webimage/',null=True)
+    name= models.CharField(max_length=70)
+    description= models.TextField()
+    link= models.CharField(max_length=200)
+    profile=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import Profile,Project
 # Create your tests here.
+class ProfileTest(TestCase):
+    def setUp(self):
+        '''
+        run before test
+        '''
+        self.profile1=Profile(name='Musyoki',bio='Software developer at Moringa School',email='msyokimutua@gmail.com')
+
+    def test_instance(self):
+        '''
+        test object initialization
+        '''
+        self.assertTrue(isinstance(self.profile1,Profile))
+
+
+    
