39c74c08ae4fa29169465c77a9c61d0eb0c1ee33
msyoki <msyokimutua@gmail.com>
2020-10-26 19:54:07 +0300

    update models

--- awwards/models.py
+++ awwards/models.py
@@ -1,27 +1,39 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
     profile_pic= models.ImageField(upload_to='profilepic/',null=True)
-    name= models.CharField(max_length=70)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
     bio= models.TextField()
     email=models.EmailField()
 
-    def __str__(self):
-        return self.name
-
+    def save_profile(self):
+        self.save()
 
 class Project(models.Model):
     webimage= models.ImageField(upload_to='webimage/',null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
     name= models.CharField(max_length=70)
     description= models.TextField()
     link= models.CharField(max_length=200)
-    profile=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+
+
+    def save_project(self):
+        self.save()
+
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        '''
+        method to search projects based on name
+        '''
+        projects=cls.objects.filter(name__icontains=search_term)
+
+        return projects
 
     def __str__(self):
         return self.name
-
 
 
 
