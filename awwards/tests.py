78772a34cd498eb9cc7e2c52998d51efd596c97f
msyoki <msyokimutua@gmail.com>
2020-10-26 19:57:42 +0300

    add  tests

--- awwards/tests.py
+++ awwards/tests.py
@@ -10,9 +10,38 @@ class ProfileTest(TestCase):
 
     def test_instance(self):
         '''
-        test object initialization
+        test profile object initialization
         '''
         self.assertTrue(isinstance(self.profile1,Profile))
 
 
-    
+    def test_save(self):
+        '''
+        test profile save method
+        '''
+        self.profile1.save()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)>=1)
+
+
+class ProjectTest(TestCase):
+    def setUp(self):
+        '''
+        run before test
+        '''
+        self.project1=Project(name='project1',description='This is a description of a test project',link='www.testproject.com')
+
+    def test_instance(self):
+        '''
+        test project object initialization
+        '''
+        self.assertTrue(isinstance(self.project1,Project))
+
+
+    def test_save(self):
+        '''
+        test profile save method
+        '''
+        self.project1.save()
+        projects=Project.objects.all()
+        self.assertTrue(len(projects)>=1)
